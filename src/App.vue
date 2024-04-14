6273882baa55235511e0988416b9597f6a14ad43
Alhuvier <al.huvier@live.fr>
2024-04-14 18:47:42 +0200

    <feat> add edit debug component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <div class="cell cell-map">
-      <MapContainer />
+      <MapContainer :geojson="geojson"></MapContainer>
     </div>
     <div class="cell cell-debug">
       <div class="cell cell-edit">
-        Edit
+        <EditDebug :geojson="geojson" v-on:change="geojson = $event">
+        </EditDebug>
       </div>
       <div class="cell cell-inspect">
         Inspect
@@ -16,11 +17,42 @@
 
 <script>
 import MapContainer from "./components/MapContainer.vue";
+import EditDebug from "./components/EditDebug.vue";
 export default {
   name: 'App',
   components: {
-    MapContainer
-  }
+    MapContainer,
+    EditDebug
+  }, data: () => ({
+    // this is the initial GeoJSON data
+    geojson: {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [
+              -27.0703125,
+              43.58039085560784
+            ],
+            [
+              -28.125,
+              23.563987128451217
+            ],
+            [
+              -10.8984375,
+              32.84267363195431
+            ],
+            [
+              -27.0703125,
+              43.58039085560784
+            ]
+          ]
+        ]
+      }
+    }
+  })
 }
 </script>
 
--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -12,14 +12,31 @@ import OSM from 'ol/source/OSM'
 // importing the OpenLayers stylesheet is required for having
 // good looking buttons!
 import 'ol/ol.css'
+import VectorSource from 'ol/source/Vector'
+import VectorLayer from 'ol/layer/Vector'
+import GeoJSON from 'ol/format/GeoJSON'
 
 export default {
     name: 'MapContainer',
     components: {},
-    props: {},
+    props: {
+        geojson: Object
+    },
+    data: () => ({
+        // store OL objects on the component instance
+        olMap: null,
+        vectorLayer: null
+    }),
     mounted() {
+
+        // a new vector layer is created with the feature
+        this.vectorLayer = new VectorLayer({
+            source: new VectorSource({
+                features: [],
+            }),
+        })
         // this is where we create the OpenLayers map
-        new Map({
+        this.olMap = new Map({
             // the map will be created using the 'map-root' ref
             target: this.$refs['map-root'],
             layers: [
@@ -27,6 +44,8 @@ export default {
                 new TileLayer({
                     source: new OSM() // tiles are served by OpenStreetMap
                 }),
+                // the vector layer is added above the tiled OSM layer
+                this.vectorLayer
             ],
 
             // the map view will initially show the whole world
@@ -36,6 +55,30 @@ export default {
                 constrainResolution: true
             }),
         })
+        // we’re calling `updateSource` to show the object initially
+        this.updateSource(this.geojson)
     },
+    watch: {
+        geojson(value) {
+            // call `updateSource` whenever the input changes as well
+            this.updateSource(value);
+        }
+    },
+    methods: {
+        // this will parse the input data and add it to the map
+        updateSource(geojson) {
+            const view = this.olMap.getView();
+            const source = this.vectorLayer.getSource();
+            const features = new GeoJSON({
+                featureProjection: 'EPSG:3857',
+            }).readFeatures(geojson)
+
+            source.clear();
+            source.addFeatures(features);
+
+            // this zooms the view on the created object
+            view.fit(source.getExtent());
+        }
+    }
 }
 </script>
